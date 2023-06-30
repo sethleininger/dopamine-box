d092349b1d557b2c3d2122f9187757ea3125b7b7
elissamaine <emaine97@yahoo.com>
2023-06-29 21:13:01 -0700

    added more to calendar page and installed font awsome

--- client/src/pages/Calendar.js
+++ client/src/pages/Calendar.js
@@ -3,10 +3,24 @@ import React from "react";
 import Calendar from 'react-calendar'
 // import styling 
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBoxopen } from '@fortawesome/free-solid-svg-icons';
+
 export default function Calendar() {
 
   return (
     <main className="calender-page">
+      <div className="key">
+        <ul>
+          <li><FontAwesomeIcon icon={faBoxopen} color="#BAE3DA"/> completed</li>
+          <li><FontAwesomeIcon icon={faBoxopen} color="#FAEDCB"/> today</li>
+          <li><FontAwesomeIcon icon={faBoxopen} color="#FAA5AB"/> incomplete</li>
+          <li><FontAwesomeIcon icon={faBoxopen} color="#F7C6A3"/> future</li>
+        </ul>
+      </div>
+      <div className="streak">
+        {/* streak info here */}
+      </div>
       <Calendar />
     </main>
   )
