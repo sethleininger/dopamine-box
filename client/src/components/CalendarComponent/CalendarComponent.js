3ee0339f0044f7f401964742640f689d90e6b883
elissamaine <emaine97@yahoo.com>
2023-07-06 19:51:11 -0700

    added calendar component and added it to the calendar page

---
+++ client/src/components/CalendarComponent/CalendarComponent.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { Calendar, dayjsLocalizer } from 'react-big-calendar'
+import dayjs from 'dayjs'
+const localizer = dayjsLocalizer(dayjs)
+
+const MyCalendar = (props) => (
+  <div>
+    <Calendar
+      localizer={localizer}
+      events={myEventsList}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500 }}
+    />
+  </div>
+)
+
+export default MyCalendar;
