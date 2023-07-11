22e2e579a508f24601f05a847bf87c090d61242e
GgeoffG <121970803+GgeoffG@users.noreply.github.com>
2023-07-10 20:01:15 -0700

    changed anonymous function, still need correct query, deleted unneccessary files

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,29 +1,36 @@
 import React, { useState } from "react";
-import Header from "../components/header/Header";
 // import Profile.css
-
-const Profile = ({ goal, tasks, username, streak }) => {
-  const [allChecked, setAllChecked] = useState(false);
-
-  const handleCheckboxChange = (index) => {
-    const updatedTasks = [...tasks];
-    updatedTasks[index].checked = !updatedTasks[index].checked;
-    const allTaskschecked = updatedTasks.every((task) => task.checked);
-    setAllChecked(allTaskschecked);
-  };
+import { useQuery } from "@apollo/client";
+import { GET_ME } from "../utils/queries";
+function Profile(tasks) {
+  // console.log("test");
+  // console.log(GET_ME);
+  // const [allChecked, setAllChecked] = useState(false);
+  const { data } = useQuery(GET_ME);
+  // console.log(data);
+  const userData = data?.me || {};
+  // console.log(userData);
+  const goal = userData.goals;
+  console.log(goal);
+  // const handleCheckboxChange = (index) => {
+  //   const updatedTasks = [...tasks];
+  //   updatedTasks[index].checked = !updatedTasks[index].checked;
+  //   const allTaskschecked = updatedTasks.every((task) => task.checked);
+  //   setAllChecked(allTaskschecked);
+  // };
   return (
     <div className="page_wrapper">
-      <Header />
       <div className="content-wrapper">
-        <h2>Welcome {username}!</h2>
-        <h3>You're {streak} days into building your habit goal!</h3>
-        <h3>Current Goal: {goal}</h3>
+        <h2>Welcome {userData.username}!</h2>
+        <h3>
+          You're {userData.goals.streak} days into building your habit goal!
+        </h3>
+        <h3>Current Goal: {userData.goals.name}</h3>
         <div className="task-box">
-          {tasks.map(({ name, index, checked }) => (
+          {/* {userData.goals[0].tasks.map(({ name, index, checked }) => (
             <>
               <label>{name}</label>
               <input
-                key={index}
                 className="task-checkbox"
                 type="checkbox"
                 name={name}
@@ -31,12 +38,12 @@ const Profile = ({ goal, tasks, username, streak }) => {
                 onChange={() => handleCheckboxChange(index)}
               ></input>
             </>
-          ))}
+          ))} */}
         </div>
         {/* {allChecked && <>effect</>} */}
       </div>
     </div>
   );
-};
+}
 
 export default Profile;
