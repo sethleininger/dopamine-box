95600a93d9730f5c2fc7cbbd57557ea0cdaea6ff
elissamaine <emaine97@yahoo.com>
2023-07-10 19:10:40 -0700

    added styling to welcome page

--- client/src/pages/Welcome/Welcome.js
+++ client/src/pages/Welcome/Welcome.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./Welcome.css";
 
-import star6 from "../../assets/star6.png";
+// import star6 from "../../assets/star6.png";
 import box2 from "../../assets/box2.svg";
 
 
@@ -11,11 +11,36 @@ export default function Welcome() {
   return (
     <main className="welcome-page">
       <div className="WP-welcome">
-        <span> <img className='star6' src={star6} alt="orange star" /> </span>
-        <h1>Welcome to <span className="WP-logo">Dopamine Box</span></h1>
-      </div>
-      <div className="box">
-        <img src={box2} alt="" />
+        <div className="welcometo">
+          <span>W</span>
+          <span>E</span>
+          <span>L</span>
+          <span>C</span>
+          <span>O</span>
+          <span>M</span>
+          <span>E</span>
+        </div>
+        <div className="to">
+          <span>T</span>
+          <span>O</span>
+        </div>  
+        <div className="dopaminebox">
+          <span>D</span>
+          <span>O</span>
+          <span>P</span>
+          <span>A</span>
+          <span>M</span>
+          <span>I</span>
+          <span>N</span>
+          <span>E</span>
+          <span>B</span>
+          <span>O</span>
+          <span>X</span>
+        </div>
+
+        <div className="box">
+          <img src={box2} alt="" />
+        </div>
       </div>
       <div>
         
