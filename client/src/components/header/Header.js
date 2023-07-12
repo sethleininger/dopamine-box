e9f561f0de972b78d0f0c855c403a5567e43cebf
andrew <andrewmuhn@gmail.com>
2023-07-12 11:31:53 -0700

    feat: PWA

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,15 +1,15 @@
-import React from "react";
-import "./header.css";
-import Auth from "../../utils/auth";
+import React from 'react';
+import './header.css';
+import Auth from '../../utils/auth';
 
-import home from "../../assets/home2.png";
-import profile from "../../assets/profile.png";
-import calendar from "../../assets/calendar.png";
-import goal from "../../assets/createGoal.png";
-import login from "../../assets/login.png";
-import logout from "../../assets/logout.png";
+import home from '../../assets/home2.png';
+import profile from '../../assets/profile.png';
+import calendar from '../../assets/calendar.png';
+import goal from '../../assets/createGoal.png';
+import login from '../../assets/login.png';
+import logout from '../../assets/logout.png';
 
-import useSound from "use-sound";
+import useSound from 'use-sound';
 import relaxClick from '../../assets/sounds/relaxClick.mp3';
 
 const signedIn = Auth.loggedIn();
@@ -29,27 +29,39 @@ const Header = ({ nav, direction, handlePageChange, ...props }) => {
   if (signedIn) {
     return (
       <header>
-        <nav className={["navbar", `navbar--${direction}`].join(" ")}>
+        <nav className={['navbar', `navbar--${direction}`].join(' ')}>
           <div>
-            <a href="#welcome" onClick={(event) => handleClick("Welcome", event)}>
+            <a
+              href="#welcome"
+              onClick={(event) => handleClick('Welcome', event)}
+            >
               <span className="icon">
                 <img src={home} alt="" />
               </span>
               <span className="pageName">Home</span>
             </a>
-            <a href="#profile" onClick={(event) => handleClick("Profile", event)}>
+            <a
+              href="#profile"
+              onClick={(event) => handleClick('Profile', event)}
+            >
               <span className="icon">
                 <img src={profile} alt="" />
               </span>
               <span className="pageName">Profile</span>
             </a>
-            <a href="#calendar" onClick={(event) => handleClick("Calendar", event)}>
+            <a
+              href="#calendar"
+              onClick={(event) => handleClick('Calendar', event)}
+            >
               <span className="icon">
                 <img src={calendar} alt="" />
               </span>
               <span className="pageName">Calendar</span>
             </a>
-            <a href="#creategoal" onClick={(event) => handleClick("CreateGoal", event)}>
+            <a
+              href="#creategoal"
+              onClick={(event) => handleClick('CreateGoal', event)}
+            >
               <span className="icon">
                 <img src={goal} alt="" />
               </span>
@@ -57,6 +69,9 @@ const Header = ({ nav, direction, handlePageChange, ...props }) => {
             </a>
           </div>
           <div>
+            <a href="#welcome" role="button" id="buttonInstall">
+              <span className="pageName">Install</span>
+            </a>
             <a href="#welcome" onClick={logOut}>
               <span className="icon">
                 <img src={logout} alt="" />
@@ -70,14 +85,17 @@ const Header = ({ nav, direction, handlePageChange, ...props }) => {
   } else if (!signedIn) {
     return (
       <header className="header2">
-        <nav className={["navbar", `navbar--${direction}`].join(" ")}>
-          <a href="#welcome" onClick={(event) => handleClick("Welcome", event)}>
+        <nav className={['navbar', `navbar--${direction}`].join(' ')}>
+          <a href="#welcome" onClick={(event) => handleClick('Welcome', event)}>
             <span className="icon">
               <img src={home} alt="" />
             </span>
             <span className="pageName">Home</span>
           </a>
-          <a href="#authform" onClick={(event) => handleClick("AuthForm", event)}>
+          <a
+            href="#authform"
+            onClick={(event) => handleClick('AuthForm', event)}
+          >
             <span className="icon">
               <img src={login} alt="" />
             </span>
