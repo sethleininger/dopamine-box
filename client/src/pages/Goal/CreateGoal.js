98e2acb8b2f4ede0fc62369ca0b7b16d868c2d12
lyskz <lyski.zach@icloud.com>
2023-07-12 12:49:30 -0700

    added goal page styling

--- client/src/pages/Goal/CreateGoal.js
+++ client/src/pages/Goal/CreateGoal.js
@@ -6,11 +6,10 @@ import "./CreateGoal.css"; // Import CreateGoal.css file
 
 // import useSound from "use-sound";
 // import signinClick from "../../assets/sounds/signinClick.mp3"
+// const [play] = useSound(signinClick);
 
 const SaveGoalForm = () => {
   const [saveGoal] = useMutation(SAVE_GOAL);
-
-  // const [play] = useSound(signinClick);
 
   const [goalFormData, setGoalFormData] = useState({
     name: "",
@@ -22,7 +21,6 @@ const SaveGoalForm = () => {
       { name: "" },
     ],
   });
-  console.log(goalFormData);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -70,90 +68,92 @@ const SaveGoalForm = () => {
   };
 
   return (
-    <>
-      <Form onSubmit={handleFormSubmit}>
-        <Form.Group className="mb-3">
-          <Form.Label htmlFor="goalName">Goal Name</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Name of Goal"
-            name="name"
-            onChange={handleInputChange}
-            value={goalFormData.name}
-            required
-          />
-        </Form.Group>
+    <div className="container">
+      <div className="form-container">
+        <Form onSubmit={handleFormSubmit}>
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="goalName" className="label-text">Goal Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Name of Goal"
+              name="name"
+              onChange={handleInputChange}
+              value={goalFormData.name}
+              required
+            />
+          </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label htmlFor="Task1">Task 1</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Your Task"
-            name="Task1"
-            onChange={handleInputChange}
-            value={goalFormData.tasks[0].name}
-            required
-          />
-        </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="Task1" className="label-text">Task 1</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Your Task"
+              name="Task1"
+              onChange={handleInputChange}
+              value={goalFormData.tasks[0].name}
+              required
+            />
+          </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label htmlFor="Task2">Task 2</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Your Task"
-            name="Task2"
-            onChange={handleInputChange}
-            value={goalFormData.tasks[1].name}
-            required
-          />
-        </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="Task2" className="label-text">Task 2</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Your Task"
+              name="Task2"
+              onChange={handleInputChange}
+              value={goalFormData.tasks[1].name}
+              required
+            />
+          </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label htmlFor="Task3">Task 3</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Your Task"
-            name="Task3"
-            onChange={handleInputChange}
-            value={goalFormData.tasks[2].name}
-            required
-          />
-        </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="Task3" className="label-text">Task 3</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Your Task"
+              name="Task3"
+              onChange={handleInputChange}
+              value={goalFormData.tasks[2].name}
+              required
+            />
+          </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label htmlFor="Task4">Task 4</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Your Task"
-            name="Task4"
-            onChange={handleInputChange}
-            value={goalFormData.tasks[3].name}
-          />
-        </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="Task4" className="label-text">Task 4</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Your Task"
+              name="Task4"
+              onChange={handleInputChange}
+              value={goalFormData.tasks[3].name}
+            />
+          </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label htmlFor="Task5">Task 5</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Your Task"
-            name="Task5"
-            onChange={handleInputChange}
-            value={goalFormData.tasks[4].name}
-          />
-        </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="Task5" className="label-text">Task 5</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Your Task"
+              name="Task5"
+              onChange={handleInputChange}
+              value={goalFormData.tasks[4].name}
+            />
+          </Form.Group>
 
-        <Button
-          disabled={
-            !goalFormData.name ||
-            !goalFormData.tasks.slice(0, 3).every((task) => task.name !== "")
-          }
-          type="submit"
-          variant="success"
-        >
-          Add a Goal
-        </Button>
-      </Form>
-    </>
+          <Button
+            disabled={
+              !goalFormData.name ||
+              !goalFormData.tasks.slice(0, 3).every((task) => task.name !== "")
+            }
+            type="submit"
+            variant="success"
+          >
+            Add Goal
+          </Button>
+        </Form>
+      </div>
+    </div>
   );
 };
 
