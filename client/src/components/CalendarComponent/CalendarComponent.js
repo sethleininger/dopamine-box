694fc44cae9caed2b0db702c53ffc4963b8a4f02
lyskz <lyski.zach@icloud.com>
2023-07-12 14:54:14 -0700

    basic calendar functioning template

--- client/src/components/CalendarComponent/CalendarComponent.js
+++ client/src/components/CalendarComponent/CalendarComponent.js
@@ -8,13 +8,21 @@ import "./CalendarComponent.css";
 const localizer = dayjsLocalizer(dayjs);
 
 const MyCalendar = ({ myEventsList, ...props }) => {
-
+  // Sample event object with start and end dates
+  const events = [
+    {
+      id: 1,
+      title: "Sample Event",
+      start: new Date(2023, 6, 12, 10, 0), // Set the start date and time
+      end: new Date(2023, 6, 12, 12, 0), // Set the end date and time
+    },
+  ];
 
   return (
     <div className="calendar-comp">
       <Calendar
         localizer={localizer}
-        events={myEventsList}
+        events={events}
         startAccessor="start"
         endAccessor="end"
         style={{ height: 500 }}
