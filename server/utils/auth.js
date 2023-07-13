5807ed5ce27e452c2542cb4058cf4f56becc23c0
GgeoffG <121970803+GgeoffG@users.noreply.github.com>
2023-07-13 15:20:11 -0700

    Process.env changed

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,16 +1,16 @@
-require('dotenv').config();
-const jwt = require('jsonwebtoken');
-const secret = process.env.SECRET;
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const secret = process.env.SESS_SECRET;
 // console.log(process);
 // console.log(secret);
-const expiration = '2h';
+const expiration = "2h";
 
 module.exports = {
   authMiddleware: function ({ req }) {
     let token = req.body.token || req.query.token || req.headers.authorization;
 
     if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+      token = token.split(" ").pop().trim();
     }
 
     if (!token) {
@@ -21,7 +21,7 @@ module.exports = {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
-      console.log('Invalid token');
+      console.log("Invalid token");
     }
 
     return req;
