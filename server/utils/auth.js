8d3f7d69b0616c5b73dba1dbba1d2b509826309f
GgeoffG <121970803+GgeoffG@users.noreply.github.com>
2023-07-13 15:30:13 -0700

    configured path for .env

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,4 +1,5 @@
-require("dotenv").config();
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "../../.env") });
 const jwt = require("jsonwebtoken");
 const secret = process.env.SESS_SECRET;
 // console.log(process);
